983e6d934bc6deab7d03147c1b52c71ef709e77f
Christoph <c@kluenter.de>
2020-12-20 22:15:03 +0100

    initial vue component

---
+++ view/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <RegistrationForm/>
+</template>
+
+<script>
+import RegistrationForm from './components/RegistrationForm'
+
+export default {
+  name: 'App',
+  components: {
+    RegistrationForm
+  }
+}
+</script>
+
+<style>
+</style>
