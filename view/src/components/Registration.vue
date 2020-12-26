5b68e2771815e77534e4ff848184d1d730334934
Christoph <c@kluenter.de>
2020-12-26 15:21:20 +0100

    registration form

--- view/src/components/Registration.vue
+++ view/src/components/Registration.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+    <h2>Registrierung</h2>
   <v-form
       ref="form"
       v-model="valid"
@@ -7,10 +8,14 @@
   >
     <v-text-field
         v-model="firstName"
+        required
+        :rules="shirtRules"
         label="Vorname"
     ></v-text-field>
     <v-text-field
         v-model="lastName"
+        required
+        :rules="shirtRules"
         label="Nachname"
     ></v-text-field>
     <v-text-field
@@ -24,6 +29,66 @@
     ></v-text-field>
 
     <v-checkbox
+      v-model="shirtWanted"
+      label="Ich will ein T-Shirt haben"
+      ></v-checkbox>
+    <v-card
+        :disabled="shirtWanted == 0"
+        ref="shirtCard"
+    >
+      <v-card-subtitle>Nur versand, keine Abholung. 15Euro</v-card-subtitle>
+      <v-card-text>
+        <v-select
+            v-model="shirtSize"
+            label="Groesse"
+            required
+            :rules="shirtRules"
+            :items=shirtSizes >
+        </v-select>
+        <v-select
+          v-model="shirtModel"
+          label="Model"
+          required
+          :rules="shirtRules"
+          :items="shirtModels">
+        </v-select>
+        <h2>Lieferanschrift</h2>
+        <v-row>
+          <v-col cols="10">
+        <v-text-field
+          v-model="street"
+          required
+          :rules="shirtRules"
+          label="Strassse"></v-text-field>
+          </v-col>
+          <v-col cols="2">
+        <v-text-field
+            v-model="streetNumber"
+            required
+            :rules="shirtRules"
+            label="Hausnummer"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-text-field
+            v-model="city"
+            required
+            :rules="shirtRules"
+            label="Stadt"></v-text-field>
+        <v-text-field
+            v-model="plz"
+            required
+            :rules="shirtRules"
+            label="PLZ"></v-text-field>
+        <v-text-field
+            v-model="country"
+            required
+            :rules="shirtRules"
+            value="Deutschland"
+            label="Land"></v-text-field>
+      </v-card-text>
+    </v-card>
+
+    <v-checkbox
         v-model="checkbox"
         :rules="[v => !!v || 'You must agree to continue!']"
         label="Do you agree?"
@@ -32,7 +97,7 @@
 
     <v-btn
         :disabled="!valid"
-        color="success"
+        color="brown"
         class="mr-4"
         @click="register"
     >
@@ -44,8 +109,7 @@
         v-model="registrationSuccessful">
       <v-card>
         <v-card-title>Registrierung erfolgreich</v-card-title>
-        <v-card-text>Den dran das Geld zu ueberwesein</v-card-text>
-        {{ registrationResult.startNumber }}
+        <v-card-text>Deine Startnummer: {{ registrationResult.startNumber }}</v-card-text>
       </v-card>
     </v-dialog>
   </v-container>
@@ -59,7 +123,17 @@
     name: 'Registration',
 
     data: () => ({
-      valid: true,
+      shirtSizes: ['XS','S','M','L'],
+      shirtSize: '',
+      shirtModels: ['Tailliert', 'Normal'],
+      shirtModel: '',
+      shirtWanted: false,
+      street: '',
+      streetNumber: '',
+      plz: '',
+      city: '',
+      country: '',
+      valid: false,
       firstName: '',
       lastName: '',
       email: '',
@@ -68,16 +142,46 @@
       registrationSuccessful: false,
       registrationResult: '',
     }),
+    computed: {
+      shirtRules () {
+        const rules = [];
+        if (this.shirtWanted) {
+          const rule =
+              v => !!v || 'Wenn Du ein Shirt willst, brauchen wir diese info';
+          rules.push(rule);
+        }
+        return rules;
+      }
+    },
+    watch: {
+     shirtWanted: 'validateField'
+    },
     methods: {
       register() {
         const url = `${API_URL}/participant/register`;
         this.$refs.form.validate();
         if (this.valid) {
-          axios.put(url, {}).then((response) => {
+          const data = {};
+          data.firstName = this.firstName;
+          data.lastName = this.lastName;
+          if(this.shirtWanted) {
+            data.shirtModel = this.shirtModel;
+            data.shirtSize = this.shirtSize;
+            data.street = this.street;
+            data.streetNumber = this.streetNumber;
+            data.plz = this.plz;
+            data.city = this.city;
+            data.country = this.country;
+          }
+          axios.put(url, data
+         ).then((response) => {
             this.registrationSuccessful = true;
             this.registrationResult = response.data;
           })
         }
+      },
+      validateField() {
+        this.$refs.form.validate()
       }
     }
   }
