f44c76c0223f6bee2225a8ede01885693a198cdc
Christoph <c@kluenter.de>
2020-12-27 14:00:51 +0100

    text for impressum/datenschutz

--- view/src/pages/impressum.vue
+++ view/src/pages/impressum.vue
@@ -1,7 +1,29 @@
 <template>
   <v-container fluid>
     <v-layout>
-      <h1>Impressum</h1>
+      <v-container>
+        <h1>Impressum</h1>
+        Anbieterangaben gem. § 5 TMG
+
+        Fußball-Club St. Pauli v. 1910 e.V.
+
+        – Abteilung: Marathon –
+
+        Harald-Stender-Platz 1
+
+        20359 Hamburg
+
+        Telefon: 040 - 31 78 74 0
+
+        E-Mail: abteilungsleitung@fcstpauli-marathon.de
+
+        Der Fußball-Club St. Pauli v. 1910 e.V. wird vertreten durch den Vorstand:
+        Oke Göttlich (Präsident), Christiane Hollander, Carsten Höltkemeyer, Joachim Pawlik, Jochen Winand
+        (Vizepräsidenten)
+
+        Registergericht: Amtsgericht Hamburg
+        Vereinsregisternummer: VR1884
+      </v-container>
     </v-layout>
   </v-container>
 </template>
