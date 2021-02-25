36725e87200ccb08393b32b52fc3993e98c4feb7
Christoph <c@kluenter.de>
2021-02-25 23:06:46 +0100

    adds google seo magic

--- view/src/components/basic/acab.vue
+++ view/src/components/basic/acab.vue
@@ -1,9 +1,11 @@
 <template>
   <v-img
       src="/fcsp_allcolorsarbeautiful.jpeg"
+      lazy-src="/fcsp_allcolorsarbeautiful_small.jpeg"
       alt="Stadion mit grossem All colors are beautiful Transpi. Foto dankenswerter weise von Stefan Groenveldt"
       class="text-right white--text align-end"
-  >&copy;Stefan Groenveldt </v-img>
+  >&copy;Stefan Groenveldt
+  </v-img>
 </template>
 <script>
 export default {
