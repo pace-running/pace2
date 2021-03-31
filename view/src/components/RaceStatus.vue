465dedaf78a041a317b5d5b6b23c5a333a71d06b
Christoph <c@kluenter.de>
2021-03-31 20:37:50 +0200

    adds note about shirt buying deadline

--- view/src/components/RaceStatus.vue
+++ view/src/components/RaceStatus.vue
@@ -5,6 +5,11 @@
       Im Mai läuft der FC St. Pauli gegen Rechts.
       Egal wo – weil es notwendig bleibt!
     </v-card-title>
+    <v-card-text>
+      Aktuelle Informationen zum T-Shirt-Versand:
+      Aufgrund der aktuellen Situation können die Shirts nur in Etappen verpackt und versandt werden.
+      Wichtig: Die Shirt-Bestellung endet am 15.5. Alle Shirts, die im Mai bestellt werden, kommen möglicherweise verzögert an.
+    </v-card-text>
     <v-card-actions>
       <v-btn x-large v-if="this.$store.state.raceOpen">
         <router-link to="registration" style="text-decoration: none; color: inherit;">Hier anmelden</router-link>
