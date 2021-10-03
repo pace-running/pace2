50b9ae2a079fbd3ee80f169724f2f3887a19985b
Christoph <c@kluenter.de>
2021-10-03 10:29:02 +0200

    bis nächstes Jahr :)

--- view/src/components/RaceStatus.vue
+++ view/src/components/RaceStatus.vue
@@ -2,14 +2,8 @@
   <v-container>
   <v-card>
     <v-card-title style="word-break: normal">
-      Im Mai läuft der FC St. Pauli gegen Rechts.
-      Egal wo – weil es notwendig bleibt!
+      St. Pauli läuft gegen Rechts. Egal wo – weil es notwendig bleibt!
     </v-card-title>
-    <v-card-text>
-      Aktuelle Informationen zum T-Shirt-Versand:
-      Aufgrund der aktuellen Situation können die Shirts nur in Etappen verpackt und versandt werden.
-      Wichtig: Die Shirt-Bestellung endet am 15.5. Alle Shirts, die im Mai bestellt werden, kommen möglicherweise verzögert an.
-    </v-card-text>
     <v-card-actions>
       <v-btn x-large v-if="this.$store.state.raceOpen">
         <router-link to="registration" style="text-decoration: none; color: inherit;">Hier anmelden</router-link>
@@ -28,9 +22,8 @@
   </v-card>
   <v-card v-if="!this.$store.state.raceOpen">
     <v-card-title>
-      Die Anmeldung für dieses Jahr öffnet am 1.3.
+      Danke an alle Teilnehmenden. Wir freuen uns auf 2022.
     </v-card-title>
-    <acab></acab>
   </v-card>
   </v-container>
 </template>
