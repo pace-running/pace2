7f33809d1f538fda70b1eb6b8ff96ef4287ebee5
Christoph <c@kluenter.de>
2021-11-13 13:03:50 +0100

    create multiple couponcodes and getByName

--- api/controllers/couponcodeController.js
+++ api/controllers/couponcodeController.js
@@ -3,7 +3,6 @@ const Couponcode = DB.Couponcode;
 const {Op} = require("sequelize");
 
 exports.get = (req, res, next) => {
-    const couponcodeName = req.params.couponcodeName
     Couponcode.findAll()
         .then(data => {
             res.send(data)
@@ -11,5 +10,49 @@ exports.get = (req, res, next) => {
         .catch(err => {
             next(err)
         })
- 
+}
+
+exports.getByName = (req,res,next) => {
+    const couponcodeName = req.params.name
+    Couponcode.findOne({
+        where: {
+            name: couponcodeName
+        }
+    }).then(data =>
+         res.send(data))
+      .catch(err=>next(err))
+}
+
+exports.create = (req, res, next) => {
+    const amount = req.body.amount;
+    let promises = []
+    const withShirt = req.body.shirt;
+    const description = req.body.description;
+    for (let i = 0; i < amount; i++) {
+       promises.push(createCouponcode(withShirt, description))
+    }
+    Promise.all(promises).then(allNames => {
+      res.status(201);
+      res.send(allNames)
+    }).catch(err => next(err))
+
+}
+
+async function createCouponcode(withShirt,description) {
+    code = await Couponcode.create( {
+        description: description,
+        shirt: withShirt,
+        used: false,
+        name: couponName()
+    })
+    return code.name
+}
+
+function couponName() {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (let i = 0; i < 8; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return 'C-' + text;
 }
--- api/routes/couponcode.js
+++ api/routes/couponcode.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const CouponcodeController = require('../controllers/couponcodeController')
 
 router.get('/', CouponcodeController.get)
+router.get('/one/:name', CouponcodeController.getByName)
+
+router.post('/create', CouponcodeController.create)
 
 module.exports = router;
