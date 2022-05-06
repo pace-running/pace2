9b1bb0ab6839f62b9364b61ed55dcbe7c192e29d
Christoph <c@kluenter.de>
2022-05-06 19:14:32 +0200

    update message on start page

--- view/src/components/RaceStatus.vue
+++ view/src/components/RaceStatus.vue
@@ -2,20 +2,14 @@
   <v-container>
   <v-card>
     <v-card-title style="word-break: normal">
-      St. Pauli läuft gegen Rechts. Egal wo – weil es notwendig bleibt!
+      Der Lauf gegen Rechts 2022 vor Ort findet am Sonntag, den 29.5.22 statt!
     </v-card-title>
     <v-card-text>
       <div class="text-body-1">
-        Es kann wie in den Jahren 2020 und 2021 dezentral, wo immer ihr wollt, gelaufen werden.
-      Dafür steht wieder der Selbstausdruck der Startnummer zu Verfügung.
+        Wir sind froh, dass es nach 2 Jahren Pandemie wieder einen Lauf gegen Rechts vor Ort, um die Alster geben kann, neben der Möglichkeit im Mai dezentral wo auch immer, laufend ein Zeichen gegen Rechts zu setzen.
       </div>
       <div class="text-body-1">
-     Der Lauf gegen Rechts 2022 kann voraussichtlich am Sonntag, den 29.5.22 stattfinden. Die endgütige Klärung, ob es, wie in den vergangenen zwei Jahren „nur“ dezentrale Läufe gegen Rechts oder auch den Lauf vor Ort, um die Alster, wie vor der Covid-Pandemie, am 29.5. gibt, wird in der ersten Mai-Woche erfolgen.
-     Über unsere Webseite und per E-Mail werden wir versuchen euch über kurzfristige Änderungen der Planung zu informieren.
-      Bestellung von T-Shirts ist im Rahmen der online Anmeldung möglich.
-      </div>
-      <div class="text-body-1 font-weight-bold">
-      Die T-Shirt Bestellung ist nur bis 1. Mai möglich.
+        Starnummernausgabe und Nachmelde-Möglichkeit findet am Samstag, 28.5.22 von 11- 17 Uhr vor den Fanräumen (Heiligengeistfeld Außenbereich der Gegengrade des Millerntor-Stadions) statt. Dort wird auch ein freier Verkauf von  restlicher T-Shirts stattfinden.
       </div>
       <div class="text-body-1">
       Anmeldung für den Lauf vor Ort, um die Alster ist bis 21.5.2022 möglich.
