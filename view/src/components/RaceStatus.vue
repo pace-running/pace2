81f8ed5c7e326432acff4a4ef51a37f4e0e54a5f
Christoph <c@kluenter.de>
2022-05-21 14:32:27 +0200

    closing online registration for 2022

--- view/src/components/RaceStatus.vue
+++ view/src/components/RaceStatus.vue
@@ -2,17 +2,22 @@
   <v-container>
   <v-card>
     <v-card-title style="word-break: normal">
-      Der Lauf gegen Rechts 2022 vor Ort findet am Sonntag, den 29.5.22 statt!
+      Die Online Anmeldung für den Lauf gegen Rechts 2022 ist geschlossen
     </v-card-title>
     <v-card-text>
       <div class="text-body-1">
-        Wir sind froh, dass es nach 2 Jahren Pandemie wieder einen Lauf gegen Rechts vor Ort, um die Alster geben kann, neben der Möglichkeit im Mai dezentral wo auch immer, laufend ein Zeichen gegen Rechts zu setzen.
+        Startnummern werden für alle, die sich online für den Lauf an der Außenalster bis zum 21.5. gemeldet hatten, speziell ausgedruckt
       </div>
       <div class="text-body-1">
-        Starnummernausgabe und Nachmelde-Möglichkeit findet am Samstag, 28.5.22 von 11- 17 Uhr vor den Fanräumen (Heiligengeistfeld Außenbereich der Gegengrade des Millerntor-Stadions) statt. Dort wird auch ein freier Verkauf von  restlicher T-Shirts stattfinden.
+      Die Startnummernausgabe findet am Samstag, 28.5.22 von 11- 17 Uhr vor den Fanräumen (Heiligengeistfeld Außenbereich der Gegengrade des Millerntor-Stadions) statt. Dort wird auch ein freier Verkauf von T-Shirts stattfinden, solang der Vorrat reicht. (Alle die ihr T-Shirt online bestellt hatten, bekommen es in der Woche nach dem 15.5. per Post zugeschickt). Wir empfehlen Startnummer bereits am Samstag abzuholen um Schlange am Lauf-Tag zu vermeiden.
       </div>
       <div class="text-body-1">
-      Anmeldung für den Lauf vor Ort, um die Alster ist bis 21.5.2022 möglich.
+      Startnummernausgabe am Sonntag, 29.5.22, ab 8.30 Uhr, Alsterwiese Schwanenwik
+      </div>
+      <div class="text-body-1">
+      Gestartet wird aus Gründen des Infektionsschutzes in 100-Personen-Blöcken ab 10 Uhr, bitte haltet im Startblock Abstand
+      Wir bitten aus solidarischen Gründen im Startbereich und auf dem Platz um das Tragen von medizinischen Masken. Bitte keine Masken auf der Laufstrecke wegwerfen.
+      Es wird Wasser an alle Läufer*innen im Zielbereich in recyclingfähigen Bechern ausgeschenkt. Bitte werft die Wasser-Becher ohne anderen Müll (sonst kann nicht recycelt werden) in die entsprechenden Tüten
       </div>
     </v-card-text>
     <v-card-actions>
