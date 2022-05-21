05ede0cfd6b78d9472b96c1eb778128a36fde90e
Christoph <c@kluenter.de>
2022-05-21 22:29:07 +0200

    pdf layout: more margin to be better cut after printing

--- api/controllers/startNumberController.js
+++ api/controllers/startNumberController.js
@@ -74,15 +74,13 @@ function appendPDF(participant, zip) {
 function generatePdf(participant) {
     let doc = new PDFDocument({size: 'A5', layout: 'landscape', margin: 0});
     doc.image(__dirname + "/../" + pathToBackgroundImage,{fit:[630,632] });
-    doc.image(__dirname + "/../" + pathToLogoRight, 515, 0, {fit: [80, 80]});
+    doc.image(__dirname + "/../" + pathToLogoRight, 510, 5, {fit: [80, 80]});
     doc.font('Helvetica-Bold').fontSize(160).fillColor("#bf82d0").text(participant.startNumber, 5, 180, {align: 'right', width: 360});
-    doc.font('Helvetica-Bold').fontSize(8).fillColor("white").text("©Stefan Groenveld", 500, 410, {align: 'center'});
+    doc.font('Helvetica-Bold').fontSize(8).fillColor("white").text("©Stefan Groenveld", 490, 405, {align: 'center'});
     if (participant.firstName) {doc.fontSize(40).fillColor('#a02dc5').text(participant.firstName, 0, 20, {align: 'center'});}
     if (participant.team) {doc.fontSize(40).fillColor('white').text(participant.team, 0, 370, {align: 'center'});}
     if (participant.hasPayed == false) {
-        doc.rotate(20)
-        doc.font('Helvetica-Bold').fontSize(40).fillColor("yellow").text("Noch nicht bezahlt", 0, 30, {align: 'center'});
-        doc.rotate(-20)
+        doc.font('Helvetica-Bold').fontSize(8).fillColor("yellow").text("noch nicht bezahlt", 0, 405, {align: 'center'});
     }
     doc.end();
     return doc
